61d0904cca88c8ee3d3c1b328cb25a45641b8e83
Артем <lemeshtema@mail.ru>
2025-12-12 03:06:24 +0300

    create gitignore

--- ScheduleParser/Program.cs
+++ ScheduleParser/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using HtmlAgilityPack;
+
+class Program
+{
+    static void Main()
+    {
+        var web = new HtmlWeb();
+        string url = "https://www.polessu.by/ruz/?f=1&q=22%D0%98%D0%A2-2";
+
+        HtmlDocument doc = web(url);
+
+        var titleNode = doc.DocumentNode.SelectSingleNode("//title");
+
+        Console.WriteLine(titleNode?.InnerText ?? "Заголовок не найден");
+    }
+}
